ea0235e359216311de68ddd2e7ebcda87377fbe0
binini11 <biniam.y.yohannes@gmail.com>
2022-12-06 19:16:10 +0000

    maxs

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list:
-        result = my_list[0]
-        for i in my_list[1:]:
-            if i > result:
-                result = i
-        return 
+    if len(my_list) == 0:
+        return (None)
+    big = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > big:
+            big = my_list[i]
+    return (big)
