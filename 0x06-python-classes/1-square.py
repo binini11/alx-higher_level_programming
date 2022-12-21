43b09027b4cdf733cf44a6f7214ad15599f6e1e9
binini11 <biniam.y.yohannes@gmail.com>
2022-12-21 08:59:22 +0000

    1 sq

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-class Square:
-    """Represent a square."""
+""" a class to define a size of square """
+
+square0 = __import__("0-square").Square
+
+class Square(square0):
+    """ an inherited class from Square definition class
+    Args:
+        square0 (class): a class which defin a square
+    """
 
     def __init__(self, size):
-        """Initialize a new Square.
+        """initialization function
         Args:
-            size (int): The size of the new square.
+            size (integer): a size attribute passed
         """
         self.__size = size
