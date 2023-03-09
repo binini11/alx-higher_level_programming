a67a3ffea296c7c8347ce3c649379883e2354978
binini11 <biniam.y.yohannes@gmail.com>
2023-03-09 18:07:28 +0000

    id = 89

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 --splays the number of records
 -- Query to display the number of records with id = 89 in a table
-SELECT COUNT(*) as id FROM first_table WHERE id=89;
+SELECT COUNT(*) FROM first_table WHERE id=89;
